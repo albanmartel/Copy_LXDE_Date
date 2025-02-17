9b2f1f41db8222fe3c33ecd75c316e8b6047d717
Alban MARTEL <albanmartel@users.noreply.github.com>
2025-02-18 00:40:09 +0100

    Update selectDate.sh
    Adds the ability to work with Wayland and x11. Adds the ability to select the current date if no calendar selection has been made

--- selectDate.sh
+++ selectDate.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 ### copy dates lxde clock lxpanel panel ###
-DATE=$(zenity --calendar --date-format "%F")
-if [[ -z "${DATE}" ]];
-then 
-	zenity --info --text "Aucune action n'a été choisie \n abandon!"
+DATE=$(zenity --calendar --text "Sélectionner une date" --date-format "%Y-%m-%d")
+if [[ -z "${DATE}" ]]; then 
+	DATE=$(date +"%Y-%m-%d")
+	zenity --info --text "No selection, \n curent date : $DATE\n is copied to the clipboard"
+else
+	zenity --info "Selected date" --text="Selected date : $DATE\n is copied to the clipboard"
+fi
+
+echo $DATE
+ 
+if [[ $XDG_SESSION_TYPE -eq wayland ]]; then
+	wl-copy $DATE
 else
 	echo "${DATE}" | xclip -i
-	xclip -o | xclip -sel clip
 fi
+
 exit
